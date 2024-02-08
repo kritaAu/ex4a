0d123d1d653b857de66e30a4c1033289628a497c
Krita <kritapat.au@gmail.com>
2024-02-08 11:18:53 +0700

    edit common.js

--- common.js
+++ common.js
@@ -1,16 +1,9 @@
 function calculate(p,d){
-		var msg = "";
+	var msg = "";
 		var pace = parseFloat(p);
 		var dist = parseInt(d);
-	//---------------------------
-		if(isNaN(pace)){
-			msg = "Please verify that the pace you have entered is correct!!!";
-			return {"status":0,"message":msg,"hour":0,"min":0};
-		}
-		if(!(pace >= 3 && pace <= 10)){
-			msg = "Running pace must be between 3 and 10 min/km";
-			return {"status":0,"message":msg,"hour":0,"min":0};
-		}
+		var chk =validate(pace);
+		if(chk !=null)return chk;
 	//---------------------------
 		var min = dist * pace;
 		var hour = 0;
@@ -21,4 +14,17 @@ function calculate(p,d){
 	//---------------------------
 		msg = "successfully calculated!"
 		return {"status":1,"message":msg,"hour":hour,"min":min};
+}
+
+function validate(pace) {
+	 
+
+	if(isNaN(pace)){
+		msg = "Please verify that the pace you have entered is correct!!!";
+		return {"status":0,"message":msg,"hour":0,"min":0};
+	}
+	if(!(pace >= 3 && pace <= 10)){
+		msg = "Running pace must be between 3 and 10 min/km";
+		return {"status":0,"message":msg,"hour":0,"min":0};
+	}
 }
